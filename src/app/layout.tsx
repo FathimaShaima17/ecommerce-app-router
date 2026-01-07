4d4631b862ff16ce325ac76a0084daee00e96d1f
shaima kk <kkshaima37@gmail.com>
2026-01-07 18:05:37 +0530

    all files added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./component/nav/Navbar";
 import { CartProvider } from "./context/CartContext";
 import Footer from "./component/footer/Footer";
 import ClientNavbarWrapper from "./clientwrapper/ClientNavbarWrapper";
